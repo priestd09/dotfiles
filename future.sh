ea47364e1462d7921791d1befd80d971edcdc217
hakanersu <darkxuma@gmail.com>
2014-04-13 21:58:30 +0300

    Update future.sh

--- future.sh
+++ future.sh
@@ -1,12 +1,12 @@
 sudo gem install sass
 
-npm install -g coffee-script
-npm install gulp
-npm install gulp-minify-css
-npm install gulp-autoprefixer
-npm install gulp-notify
-npm install gulp-ruby-sass 
-npm install gulp-coffee
-npm install gulp-concat
-npm install gulp-uglify
-npm install gulp-imagemin
+sudo npm install -g coffee-script
+sudo npm install gulp
+sudo npm install gulp-minify-css
+sudo npm install gulp-autoprefixer
+sudo npm install gulp-notify
+sudo npm install gulp-ruby-sass 
+sudo npm install gulp-coffee
+sudo npm install gulp-concat
+sudo npm install gulp-uglify
+sudo npm install gulp-imagemin
