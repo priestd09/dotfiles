d81ee59bbf0128450361b2e516b652e6f9af817f
hakanersu <darkxuma@gmail.com>
2014-04-13 21:37:15 +0300

    Will be added soon after tests

---
+++ future.sh
@@ -0,0 +1,11 @@
+sudo gem install sass
+
+npm install gulp
+npm install gulp-minify-css
+npm install gulp-autoprefixer
+npm install gulp-notify
+npm install gulp-ruby-sass 
+npm install gulp-coffee
+npm install gulp-concat
+npm install gulp-uglify
+npm install gulp-imagemin
