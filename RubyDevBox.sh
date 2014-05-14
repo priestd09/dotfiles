98245a0a51d73c101ed9743b93240f64672fc9a6
Hakan ERSU <hakanersu@gmail.com>
2014-05-14 14:13:01 +0300

    Compile Ruby from source

--- RubyDevBox.sh
+++ RubyDevBox.sh
@@ -21,15 +21,23 @@ wget http://nodejs.org/dist/v0.10.24/node-v0.10.24.tar.gz
 tar -xvf node-v0.10.24.tar.gz
 cd node-v0.10.24 && ./configure && make && sudo make install
 
-echo "RVM KURULUMUNA GECELIM"
-sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
-curl -L https://get.rvm.io | bash -s stable
-echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile 
-source ~/.rvm/scripts/rvm
-type rvm | head -n 1
-rvm install 2.1.2 && rvm use 2.1.2 --default && ruby -v && gem install rails --pre
+# echo "RVM KURULUMUNA GECELIM"
+# sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+# curl -L https://get.rvm.io | bash -s stable
+# echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile 
+# source ~/.rvm/scripts/rvm
+# type rvm | head -n 1
+# rvm install 2.1.2 && rvm use 2.1.2 --default && ruby -v && gem install rails --pre
+# ruby -v
+echo "RUBY'I DERLEYELIM RVM ILE ILGILI SIKINTILAR BULUNMAKTA"
+cd
+wget http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz
+tar -xzvf ruby-2.1.2.tar.gz
+cd ruby-2.1.2/
+./configure
+make
+sudo make install
 ruby -v
-
 
 echo "RAILS KURULUMU"
 gem install rails
@@ -41,6 +49,15 @@ sudo apt-get update
 sudo apt-get install -y postgresql-common
 sudo apt-get install -y postgresql-9.3 libpq-dev
 
+echo "MYSQL KURULUMU"
+sudo apt-get install mysql-server mysql-client libmysqlclient-dev
+
+
 echo "QT YUKLENIYOR"
 sudo apt-get install -y qt4-qmake
 sudo apt-get install -y libqtwebkit-dev
+
+git clone https://github.com/bulutfon/bulutfon.git
+
+cd bulutfon
+bundle
